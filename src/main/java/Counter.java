31643847e544ae1f449e03198d677db9aedffb70
“Ahmad <ahmadjunaidibeds@gmail.com>
2025-05-30 16:16:18 +0700

    fix: simplify Counter initialization and update Driver output messages for clarity

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,25 +1,24 @@
 public class Counter {
 
     private int count;
-    
+
     public Counter() {
-        this.reset();
+        count = 5;
     }
-        
+
     public void reset() {
         count = 5;
     }
-    
+
     public void increment() {
         count++;
     }
-    
+
     public void decrement() {
         count--;
     }
-    
+
     public int getCount() {
         return count;
     }
-    
 }
--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -3,18 +3,13 @@ public class Driver {
     public static void main(String[] args) {
         
         Counter counter = new Counter();
-        
-        System.out.println("Hitung:" + counter.getCount());
-        
-        counter.increase(5);
-        
-        System.out.println("Hitung:" + counter.getCount());
-        
-        counter.decrease(2);
-        
-        System.out.println("Hitung:" + counter.getCount());
-        
-        counter.multiply(6);
-    }
 
+        System.out.println("Current count: " + counter.getCount());
+
+        counter.increment();
+        System.out.println("Current count: " + counter.getCount());
+
+        counter.decrement();
+        System.out.println("Current count: " + counter.getCount());
+    }
 }
