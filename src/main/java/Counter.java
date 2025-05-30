759d8290d3d4a5e6ab6fd94f2e0977bae06dfd93
“Ahmad <ahmadjunaidibeds@gmail.com>
2025-05-30 16:39:03 +0700

    fix: clean up formatting and add missing methods in Counter class

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,26 +1,36 @@
 public class Counter {
 
     private int count;
-    
+
     public Counter() {
         this.reset();
     }
-        
+
     public void reset() {
         count = 0;
     }
-    
+
     public void increment() {
         count++;
     }
-    
+
     public void decrement() {
         count--;
     }
-    
+
+    public void increase(int amount) {
+        count += amount;
+    }
+
+    public void decrease(int amount) {
+        count -= amount;
+    }
+
+    public void multiply(int factor) {
+        count *= factor;
+    }
+
     public int getCount() {
         return count;
     }
-    
 }
-
