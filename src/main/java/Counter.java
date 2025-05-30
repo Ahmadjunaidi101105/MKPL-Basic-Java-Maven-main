90f73d5b7bb391a45d1758bcb7c33eed554968fa
“Ahmad <ahmadjunaidibeds@gmail.com>
2025-05-30 16:24:26 +0700

    fix: add missing methods to Counter class for enhanced functionality

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,26 +1,37 @@
 public class Counter {
 
     private int count;
-    
+
     public Counter() {
         this.reset();
     }
-        
+
     public void reset() {
         count = 5;
     }
-    
+
     public void increment() {
         count++;
     }
-    
+
     public void decrement() {
         count--;
     }
-    
+
     public int getCount() {
         return count;
     }
-    
-}
 
+    // Add the missing methods
+    public void increase(int value) {
+        count += value;
+    }
+
+    public void decrease(int value) {
+        count -= value;
+    }
+
+    public void multiply(int value) {
+        count *= value;
+    }
+}
